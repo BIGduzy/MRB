16e3b5b0279ba408a83c6c66b72726b62b12df07
BIGduzy <nick-b@live.nl>
2018-06-10 14:29:57 +0200

    Added arduino code

---
+++ MRB.ino
@@ -0,0 +1,27 @@
+bool firstByte = true;
+uint8_t incomingByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    //Serial.print("I received: ");
+    //Serial.println((char)incomingByte);
+
+    if (incomingByte == 'M') {
+        while(Serial.available() == 0) {}; // Wait for data
+        uint8_t data = Serial.read();
+        Serial.print("Value: ");
+        Serial.println(data);
+    }
+  }
+  delay(1);
+}
