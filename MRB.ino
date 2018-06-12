c2a21170cbaf8e9084ac8ba18ce41db810886a76
Niels de Waal <nielsdwaal@gmail.com>
2018-06-12 14:23:46 +0200

    Speaker to me bb

--- MRB.ino
+++ MRB.ino
@@ -1,10 +1,24 @@
 const int fan = 7;
+const int speaker = 8;
 bool firstByte = true;
 uint8_t incomingByte = 0;
+
+int convertByteToSpeaker(uint8_t dat) {
+    int ret = (dat * 12) + 2000; // Need to get a value between 2k and 5k
+
+    if (ret > 5000) {
+        ret = 5000;
+    } else if (ret < 2000) {
+        ret = 2000;
+    }
+
+    return ret;
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(fan, OUTPUT);
+  pinMode(speaker, OUTPUT);
 }
 
 
@@ -24,6 +38,10 @@ void loop() {
         //Serial.print("Value: ");
         //Serial.println(data);
         analogWrite(fan, data);
+    } else if (incomingByte == "S") {
+        while(Serial.available() == 0) {};
+        uint8_t data = Serial.read();
+        tone(speaker, convertByteToSpeaker(data), 200);
     }
   }
   delay(1);
